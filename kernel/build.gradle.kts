075a94ff3b79bf2405e310529e1056996ea0ed67
DevKoenv <koenvanvlimmeren@gmail.com>
2026-05-31 01:47:27 +0200

    fix(kernel): remove logback-classic from abstraction module

--- kernel/build.gradle.kts
+++ kernel/build.gradle.kts
@@ -2,7 +2,6 @@ dependencies {
     implementation(project(":shared"))
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.ktor.client.core)
-    implementation(libs.logback.classic)
 
     testImplementation(libs.bundles.testing)
 }
