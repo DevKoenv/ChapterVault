67648359e5747c7f6f9ebf6911b38dc4bfaa7a5a
DevKoenv <koenvanvlimmeren@gmail.com>
2026-06-02 23:48:32 +0200

    fix(sse): suppress IOException on client disconnect in /events stream

--- interfaces/src/main/kotlin/dev/koenv/chaptervault/interfaces/api/sse/SseRoutes.kt
+++ interfaces/src/main/kotlin/dev/koenv/chaptervault/interfaces/api/sse/SseRoutes.kt
@@ -3,11 +3,16 @@ package dev.koenv.chaptervault.interfaces.api.sse
 import dev.koenv.chaptervault.interfaces.api.websocket.EventProjectionService
 import io.ktor.server.routing.Route
 import io.ktor.server.sse.sse
+import java.io.IOException
 
 fun Route.sseRoutes(projectionService: EventProjectionService) {
     sse("/events") {
-        projectionService.events.collect { event ->
-            send(event)
+        try {
+            projectionService.events.collect { event ->
+                send(event)
+            }
+        } catch (_: IOException) {
+            // Client disconnected; stop sending silently.
         }
     }
 }
